fbe82885f7afba1ff0f13616f33dacfe739303ca
A5b84 <42934711+A5b84@users.noreply.github.com>
2021-04-10 18:14:54 +0200

    Update stuff

---
+++ README.md
@@ -0,0 +1,3 @@
+# Interdimensional Map Markers
+
+A server-side [Fabric](https://fabricmc.net/) mod that makes maps display markers for players in other dimensions too.
--- src/main/java/io/github/a5b84/interdimensionalmapmarkers/mixin/MapStateMixin.java
+++ src/main/java/io/github/a5b84/interdimensionalmapmarkers/mixin/MapStateMixin.java
@@ -1,7 +1,11 @@
 package io.github.a5b84.interdimensionalmapmarkers.mixin;
 
-import javax.annotation.Nullable;
-
+import net.minecraft.item.map.MapIcon.Type;
+import net.minecraft.item.map.MapState;
+import net.minecraft.text.Text;
+import net.minecraft.util.registry.RegistryKey;
+import net.minecraft.world.World;
+import net.minecraft.world.WorldAccess;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
@@ -12,68 +16,51 @@ import org.spongepowered.asm.mixin.injection.ModifyConstant;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-import net.minecraft.item.map.MapIcon.Type;
-import net.minecraft.item.map.MapState;
-import net.minecraft.text.Text;
-import net.minecraft.util.registry.RegistryKey;
-import net.minecraft.world.World;
-import net.minecraft.world.WorldAccess;
-
 @Mixin(MapState.class)
 public abstract class MapStateMixin {
 
     @Shadow public RegistryKey<World> dimension;
 
-    @Unique private @Nullable World mapWorld;
-    @Unique private @Nullable World markerWorld;
+    @Unique private World mapWorld;
+    @Unique private World markerWorld;
     @Unique private double coordinateScale;
     @Unique private double markerRotation;
 
 
 
-    /**
-     * Permet de mettre à jour les icônes de joueurs dans les mauvaises
-     * dimensions
-     * @see MapState#update
-     */
+    /** Allows updating icons of players in other dimensions */
     @Redirect(method = "update",
         at = @At(value = "INVOKE", target = "net/minecraft/world/World.getRegistryKey()Lnet/minecraft/util/registry/RegistryKey;"))
     private RegistryKey<World> playerDimensionProxy(World world) {
         return dimension;
     }
 
-    /** Déplace les marqueurs de joueurs dans les mauvaises dimensions
-     * @see MapState#addIcon */
+    /** Stores the method parameters and adjusts the X coordinate */
     @ModifyVariable(method = "addIcon", at = @At("HEAD"), ordinal = 0)
-    private double adjustX(double x, Type type, @Nullable WorldAccess world, String key, double _x, double z, double rotation) {
+    private double adjustX(double x, Type type, WorldAccess world, String key, double _x, double z, double rotation) {
         markerWorld = world instanceof World ? (World) world : null;
         markerRotation = rotation;
         coordinateScale = getCoordinateScale();
         return x * coordinateScale;
     }
 
-    /**
-     * Déplace les marqueurs de joueurs dans les mauvaises dimensions
-     * et enregistre le monde du marqueur et son orientation
-     * @see MapState#addIcon
-     */
+    /** Adjusts the Z coordinate */
     @ModifyVariable(method = "addIcon", at = @At("HEAD"), ordinal = 1)
     private double adjustZ(double z) {
         return z * coordinateScale;
     }
 
-    /** Augmente la distance max des marqueurs selon les dimensions */
+    /** Increases the off map distance */
     @ModifyConstant(method = "addIcon", constant = @Constant(floatValue = 320))
-    private float adjustOffMapDistance(float d) {
-        return d * Math.max((float) coordinateScale, 1);
+    private float adjustOffMapDistance(float offMapDistance) {
+        return offMapDistance * Math.max((float) coordinateScale, 1);
     }
 
-    /** Recolore les icônes de joueurs dans les mauvaises dimensions
-     * @see MapState#addIcon */
+    /** Recolors the icons of player in other dimensions */
     @ModifyArg(method = "addIcon",
         at = @At(value = "INVOKE", target = "net/minecraft/item/map/MapIcon.<init>(Lnet/minecraft/item/map/MapIcon$Type;BBBLnet/minecraft/text/Text;)V"), index = 0)
-    private Type adjustMarkerType(Type type, byte x, byte z, byte rotation, @Nullable Text text) {
-        // On modifie que les icônes de joueurs
+    private Type adjustMarkerType(Type type, byte x, byte z, byte rotation, Text text) {
+        // Check for player icons
         if (type != Type.PLAYER
                 && type != Type.PLAYER_OFF_MAP
                 && type != Type.PLAYER_OFF_LIMITS
@@ -81,46 +68,39 @@ public abstract class MapStateMixin {
             return type;
         }
 
-        final RegistryKey<World> markerKey = markerWorld.getRegistryKey();
-        if (markerKey == dimension) return type;
+        final RegistryKey<World> markerDimension = markerWorld.getRegistryKey();
+        if (markerDimension == dimension) return type;
 
-        if (markerKey == World.OVERWORLD) return Type.FRAME; // Marqueur vert
-        if (markerKey == World.NETHER) return Type.RED_MARKER;
-        if (markerKey == World.END) return Type.BLUE_MARKER;
+        if (markerDimension == World.OVERWORLD) return Type.FRAME; // Green marker
+        if (markerDimension == World.NETHER) return Type.RED_MARKER;
+        if (markerDimension == World.END) return Type.BLUE_MARKER;
 
         return type;
     }
 
-    /** Recolore les icônes de joueurs dans les mauvaises dimensions
-     * @see MapState#addIcon */
+    /** Recolors player markers from other dimensions */
     @ModifyArg(method = "addIcon",
         at = @At(value = "INVOKE", target = "net/minecraft/item/map/MapIcon.<init>(Lnet/minecraft/item/map/MapIcon$Type;BBBLnet/minecraft/text/Text;)V"), index = 3)
-    private byte adjustMarkerRotation(Type type, byte x, byte z, byte rotation, @Nullable Text text) {
+    private byte adjustMarkerRotation(Type type, byte x, byte z, byte rotation, Text text) {
         if (rotation == 0
                 && type != Type.PLAYER_OFF_MAP
                 && type != Type.PLAYER_OFF_LIMITS
                 && (x == -128 || x == 127 || z == -128 || z == 127)) {
-            // Marqueur sans orientation et sur un bord -> il était
-            // propablement hors de la carte et c'est pas la vrai orientation
-            // -> on la recalcule
-            // (Calcul "inspiré" de MapState#addIcon)
+            // Recalculate the rotation if the marker was off-map
             markerRotation += markerRotation < 0 ? -8 : 8;
             return (byte) (markerRotation * 16 / 360);
         }
 
-        // Sinon c'est bon
         return rotation;
     }
 
 
 
-    /** Renvoie le ratio de distances entre la dimension en paramètre et celle
-     * de la carte */
     @Unique private double getCoordinateScale() {
         if (markerWorld == null) return 1;
 
-        // On récupère le monde de la carte si on a pas le bon
         if (mapWorld == null || mapWorld.getRegistryKey() != dimension) {
+            //noinspection ConstantConditions
             mapWorld = markerWorld.getServer().getWorld(dimension);
             if (mapWorld == null) return 1;
         }
