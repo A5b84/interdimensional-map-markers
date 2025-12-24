9724fb01820eb341da243d6cd630c30a13c23706
A5b84 <42934711+A5b84@users.noreply.github.com>
2025-12-24 21:05:41 +0100

    Migrate to Mojang mappings

--- src/main/java/io/github/a5b84/interdimensionalmapmarkers/mixin/MapItemSavedDataMixin.java
+++ src/main/java/io/github/a5b84/interdimensionalmapmarkers/mixin/MapItemSavedDataMixin.java
@@ -1,14 +1,14 @@
 package io.github.a5b84.interdimensionalmapmarkers.mixin;
 
 import java.util.Optional;
-import net.minecraft.item.map.MapDecorationType;
-import net.minecraft.item.map.MapDecorationTypes;
-import net.minecraft.item.map.MapState;
-import net.minecraft.registry.RegistryKey;
-import net.minecraft.registry.entry.RegistryEntry;
-import net.minecraft.text.Text;
-import net.minecraft.world.World;
-import net.minecraft.world.WorldAccess;
+import net.minecraft.core.Holder;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.saveddata.maps.MapDecorationType;
+import net.minecraft.world.level.saveddata.maps.MapDecorationTypes;
+import net.minecraft.world.level.saveddata.maps.MapItemSavedData;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -20,25 +20,25 @@ import org.spongepowered.asm.mixin.injection.ModifyConstant;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-@Mixin(MapState.class)
-public abstract class MapStateMixin {
+@Mixin(MapItemSavedData.class)
+public abstract class MapItemSavedDataMixin {
 
-  @Shadow public @Final RegistryKey<World> dimension;
+  @Shadow public @Final ResourceKey<Level> dimension;
 
-  @Unique private World mapWorld;
-  @Unique private World markerWorld;
+  @Unique private Level mapWorld;
+  @Unique private Level markerWorld;
   @Unique private double coordinateScale;
   @Unique private double markerRotation;
 
   /** Allows updating icons of players in other dimensions */
   @Redirect(
-      method = "update",
+      method = "tickCarriedBy",
       at =
           @At(
               value = "INVOKE",
               target =
-                  "net/minecraft/world/World.getRegistryKey()Lnet/minecraft/registry/RegistryKey;"))
-  private RegistryKey<World> playerDimensionProxy(World world) {
+                  "Lnet/minecraft/world/level/Level;dimension()Lnet/minecraft/resources/ResourceKey;"))
+  private ResourceKey<Level> playerDimensionProxy(Level world) {
     return dimension;
   }
 
@@ -46,13 +46,13 @@ public abstract class MapStateMixin {
   @ModifyVariable(method = "addDecoration", at = @At("HEAD"), ordinal = 0, argsOnly = true)
   private double adjustX(
       double x,
-      RegistryEntry<MapDecorationType> type,
-      WorldAccess worldAccess,
+      Holder<MapDecorationType> type,
+      LevelAccessor worldAccess,
       String key,
       double _x,
       double z,
       double rotation) {
-    markerWorld = worldAccess instanceof World world ? world : null;
+    markerWorld = worldAccess instanceof Level world ? world : null;
     markerRotation = rotation;
     coordinateScale = getCoordinateScale();
     return x * coordinateScale;
@@ -65,7 +65,9 @@ public abstract class MapStateMixin {
   }
 
   /** Increases the off map distance */
-  @ModifyConstant(method = "getPlayerMarker", constant = @Constant(floatValue = 320))
+  @ModifyConstant(
+      method = "decorationTypeForPlayerOutsideMap",
+      constant = @Constant(floatValue = 320))
   private float adjustOffMapDistance(float offMapDistance) {
     return offMapDistance * Math.max((float) coordinateScale, 1);
   }
@@ -78,27 +80,27 @@ public abstract class MapStateMixin {
           @At(
               value = "INVOKE",
               target =
-                  "net/minecraft/item/map/MapDecoration.<init>(Lnet/minecraft/registry/entry/RegistryEntry;BBBLjava/util/Optional;)V"),
+                  "Lnet/minecraft/world/level/saveddata/maps/MapDecoration;<init>(Lnet/minecraft/core/Holder;BBBLjava/util/Optional;)V"),
       index = 0)
-  private RegistryEntry<MapDecorationType> adjustMarkerMapDecorationType(
-      RegistryEntry<MapDecorationType> type, byte x, byte z, byte rotation, Optional<Text> name) {
+  private Holder<MapDecorationType> adjustMarkerMapDecorationType(
+      Holder<MapDecorationType> type, byte x, byte z, byte rotation, Optional<Component> name) {
     // Check for player icons
-    //noinspection deprecation (type.matches)
+    // noinspection deprecation (type.is)
     if (markerWorld == null
-        || !type.matches(MapDecorationTypes.PLAYER)
-            && !type.matches(MapDecorationTypes.PLAYER_OFF_MAP)
-            && !type.matches(MapDecorationTypes.PLAYER_OFF_LIMITS)) {
+        || !type.is(MapDecorationTypes.PLAYER)
+            && !type.is(MapDecorationTypes.PLAYER_OFF_MAP)
+            && !type.is(MapDecorationTypes.PLAYER_OFF_LIMITS)) {
       return type;
     }
 
-    RegistryKey<World> markerDimension = markerWorld.getRegistryKey();
+    ResourceKey<Level> markerDimension = markerWorld.dimension();
     if (markerDimension == dimension) {
       return type;
-    } else if (markerDimension == World.OVERWORLD) {
+    } else if (markerDimension == Level.OVERWORLD) {
       return MapDecorationTypes.FRAME; // Green marker
-    } else if (markerDimension == World.NETHER) {
+    } else if (markerDimension == Level.NETHER) {
       return MapDecorationTypes.RED_MARKER;
-    } else if (markerDimension == World.END) {
+    } else if (markerDimension == Level.END) {
       return MapDecorationTypes.BLUE_MARKER;
     } else {
       return type;
@@ -113,14 +115,14 @@ public abstract class MapStateMixin {
           @At(
               value = "INVOKE",
               target =
-                  "net/minecraft/item/map/MapDecoration.<init>(Lnet/minecraft/registry/entry/RegistryEntry;BBBLjava/util/Optional;)V"),
+                  "Lnet/minecraft/world/level/saveddata/maps/MapDecoration;<init>(Lnet/minecraft/core/Holder;BBBLjava/util/Optional;)V"),
       index = 3)
   private byte adjustMarkerRotation(
-      RegistryEntry<MapDecorationType> type, byte x, byte z, byte rotation, Optional<Text> name) {
+      Holder<MapDecorationType> type, byte x, byte z, byte rotation, Optional<Component> name) {
     //noinspection deprecation (type.matches)
     if (rotation == 0
-        && !type.matches(MapDecorationTypes.PLAYER_OFF_MAP)
-        && !type.matches(MapDecorationTypes.PLAYER_OFF_LIMITS)
+        && !type.is(MapDecorationTypes.PLAYER_OFF_MAP)
+        && !type.is(MapDecorationTypes.PLAYER_OFF_LIMITS)
         && (x == -128 || x == 127 || z == -128 || z == 127)) {
       markerRotation += markerRotation < 0 ? -8 : 8;
       return (byte) (markerRotation * 16 / 360);
@@ -135,14 +137,15 @@ public abstract class MapStateMixin {
       return 1;
     }
 
-    if (mapWorld == null || mapWorld.getRegistryKey() != dimension) {
+    if (mapWorld == null || mapWorld.dimension() != dimension) {
       //noinspection ConstantConditions
-      mapWorld = markerWorld.getServer().getWorld(dimension);
+      mapWorld = markerWorld.getServer().getLevel(dimension);
       if (mapWorld == null) {
         return 1;
       }
     }
 
-    return markerWorld.getDimension().coordinateScale() / mapWorld.getDimension().coordinateScale();
+    return markerWorld.dimensionType().coordinateScale()
+        / mapWorld.dimensionType().coordinateScale();
   }
 }
